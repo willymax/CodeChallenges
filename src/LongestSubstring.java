b96270789463338e5b2ddeb687d9369c1380c22c
William Makau <williammakau070@gmail.com>
2022-06-18 10:06:14 +0300

    longest substring

---
+++ src/LongestSubstring.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        LongestSubstring longestSubstring = new LongestSubstring();
+        System.out.println(longestSubstring.lengthOfLongestSubstring(" "));
+        ;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put(' ', 22);
+        System.out.println(map.get(' '));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        for (int i = 0, j = 0; i < s.length(); ++i) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+}
